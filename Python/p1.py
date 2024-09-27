255460e8a253066d45f59350eb19a228956a4da9
Sumit <121432275+Sumit444-commits@users.noreply.github.com>
2024-09-27 23:01:38 +0500

    commit

---
+++ Python/p1.py
@@ -0,0 +1,42 @@
+# a="5"
+# b = input()
+# # b=int(input())
+
+# # print(a + "helo")
+# print(f"helo{a}")
+
+
+# if a>b:
+#     print("a is greater than b")
+# else:
+#     print("b is greater than a")
+    
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# print(lst[0])
+# print(lst[-1])
+# print(lst[-10])
+
+# for i in range(5): 
+#     if i == 3: pass  # Does nothing when i equals 3
+#     print(i)
+
+
+# def fun(arg=2):
+#     print(arg)
+#     return arg
+
+# print(fun(1) + fun())  # Output: 1 2 3
+
+# def my_function(**kid):
+#   print("His last name is " + kid["lname"])
+
+# my_function(fname = "Tobias", lname = "Refsnes")
+
+# def my_function(x):
+#   print(x)
+
+# my_function(x = 3)
+
+from math import sqrt
+
+print(sqrt(4))
---
+++ Python/wordcount.py
@@ -0,0 +1,13 @@
+text = '''In this rapidly growing technical world, working effectively is a must. Acquiring skills has always been a priority to me, and I love facing new challenges, and I believe that doing so will sharpen my skills. Of course I believe with strong dedication, discipline, and smart work I can achieve any goal, and I found myself as a goal-oriented person.
+I have always been a creative and active student, competing in quizzes, speeches, sports, and project competitions. Not only video games, I also have won badminton and chess competitions.
+As a software developer, my curiosity makes me explore effective solutions and build something unique on my own. As in my academic journey, I have worked on different programming languages (like CPP, Java, JavaScript, and Python) and also have worked on real-time web projects as a frontend developer, but as we know, the learning process never ends, so I constantly seek to learn something new. Now I have started learning AI, and I have made a goal to contribute in the field of AI and bring it to the next level.
+USA has richly adopted AI; companies like Google, Microsoft, and OpenAI have contributed a lot in this field. Today AI is approximately everywhere.
+This is an ideal opportunity for me to learn and enhance my AI coding skills, which will ultimately benefit me in taking the next step to building my own empire. UGRAD can give me the ability to break out of my limitations and serve entire humanity.'''
+
+count = 0
+
+for i in text:
+    if i == " ":
+        count += 1
+
+print(count)
